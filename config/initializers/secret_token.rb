dcb68392b7e55d5c6b9d2409529297c312863907
voidstar <brett.c.stewart@gmail.com>
2011-10-22 07:20:38 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sundial::Application.config.secret_token = '8189f662a00612ad7a5b7bd82059d8c8267e1f5910aca4c9aca6011c311fde959483981948a73eb8f22e108778ba82b5a9e172b0946fb93549957fb59c8775f2'
